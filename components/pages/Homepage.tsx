db1c0569e39080911925c5bd6f816308b1bb43d4
SupportJixart <support@jixart.fr>
2022-02-28 13:40:50 +0100

    création menu

---
+++ components/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const Homepage: React.FC = () => {
+  return (
+      <View>
+          <Text>
+              HOMEPAGE
+          </Text>
+      </View>
+  );
+};
+
+export default Homepage;
