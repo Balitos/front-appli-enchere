2ea5bc22064563d4fbd90cc8aba0392264cf6aad
Nicolas <onprojetpasnous@gmail.com>
2022-04-26 09:33:53 +0200

    init card

--- components/pages/Homepage.tsx
+++ components/pages/Homepage.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import Card  from '../Card/Card';
 
 
 const Homepage: React.FC = () => {
+    const [product, setProduct] = useState([
+        {name: 'Chaussure', price: '23', key: '1'},
+        {name: 'Voiture', price: '23', key: '2'},
+        {name: 'Tee-shirt', price: '23', key: '3'},
+        {name: 'Helicoptere', price: '23', key: '4'},
+        {name: 'Blousson',  price: '23', key: '5'},
+    ])
   return (
       <View>
-          <Text>
-              HOMEPAGE
-          </Text>
+          <ScrollView>
+              {
+                  product.map(item => (
+                          <Card title={item.name} price={item.price} key={item.key}/>
+                  ))
+              }
+          </ScrollView>
       </View>
   );
 };
